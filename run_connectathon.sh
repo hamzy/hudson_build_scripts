e8ec2b3df7954ee8b620b19914b2e3995b26b208
Sean Dague <sean@dague.net>
2010-11-19 11:46:07 -0500

    fix connectathon builder

--- run_connectathon.sh
+++ run_connectathon.sh
@@ -9,7 +9,14 @@
 ## Last update Fri Nov 19 11:12:16 2010 Sean Dague
 ##
 
+echo "Building connectathon"
+if [[ -d /home/hudson/cthon04 ]]; then
+    git clone git://morbo.linux.ibm.com/~sdague/cthon04 /home/hudson/cthon04
+fi
 cd /home/hudson/cthon04
+git pull
+make
+
 echo "Unmounting everything"
 sudo killall runcthon
 sudo killall tlocklfs
@@ -18,6 +25,7 @@ ssh -tt root@sonas12 service nfs-ganesha-gpfs restart
 sudo ./runcthon --server sonas12 --serverdir /ibm/fs0/hudson/$NODE --onlyv3
 sudo ./runcthon --unmountall
 
+echo "Running the parser"
 # get the parse
 if [[ ! -d /home/hudson/cthon2junit ]]; then
     git clone git://morbo.linux.ibm.com/~sdague/cthon2junit /home/hudson/cthon2junit
