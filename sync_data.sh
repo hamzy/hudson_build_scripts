683762286e370875417d1cf0eaff62126e5591f2
Sean Dague <sean@dague.net>
2011-03-07 15:00:09 -0500

    fix to work with stephenson

--- sync_data.sh
+++ sync_data.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ##
 ## sync_data.sh
-## 
+##
 ## Made by Sean Dague
 ## Login   <sdague@orac>
-## 
+##
 ## Started on  Mon Feb 14 10:05:04 2011 Sean Dague
 ## Last update Mon Feb 14 10:05:04 2011 Sean Dague
 ##
 
 # pull in the hosts file
 
-if [[ $HOSTNAME = "herbert" ]]; then
+if [[ $HOSTNAME = "stephenson.pok.ibm.com" ]]; then
     exit 0
 fi
 
