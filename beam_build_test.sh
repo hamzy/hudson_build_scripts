26f29a8e367dda558e3e7b3ddf1ee1f5f80a3657
Sean Dague <sdague@linux.vnet.ibm.com>
2011-04-27 15:35:59 -0400

    Adding beam build test

---
+++ beam_build_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+##
+## enhanced_build_test.sh
+## 
+## Made by Sean Dague
+## Login   <sdague@orac>
+##
+## This does a build with and without memcorrupt and runs unit tests
+## and packaging tests.  Used for a standard build of Ganesha, takes
+## about 12 minutes.
+## 
+## Started on  Fri Nov 19 09:35:17 2010 Sean Dague
+## Last update Fri Nov 19 09:35:17 2010 Sean Dague
+##
+
+cd src
+export CONFIG_FLAGS="--with-fsal=GPFS --enable-snmp-adm --enable-nlm --enable-stat-exporter"
+autoreconf -i
+CC=beam-cc ./configure $CONFIG_FLAGS
+make clean
+make
