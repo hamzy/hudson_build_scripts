2114cbb325b596b7e525b2b52186fe4cdb468783
Sean Dague <sean@dague.net>
2011-07-22 13:22:31 -0400

    wrong syntax for host

--- run_connectathon64.sh
+++ run_connectathon64.sh
@@ -30,16 +30,16 @@ echo "Unmounting everything"
 sudo killall runcthon
 sudo killall tlocklfs
 sudo ./runcthon --unmountall
-ssh -tt root@$(HOST) service nfs-ganesha-gpfs restart || exit 1
-sudo ./runcthon --server $(HOST) --serverdir /ibm/fs0/hudson64/root/$NODE_NAME --onlyv3
+ssh -tt root@$HOST service nfs-ganesha-gpfs restart || exit 1
+sudo ./runcthon --server $HOST --serverdir /ibm/fs0/hudson64/root/$NODE_NAME --onlyv3
 sudo ./runcthon --unmountall
 
 echo "Unmounting everything"
 sudo killall runcthon
 sudo killall tlocklfs
 sudo ./runcthon --unmountall
-ssh -tt root@$(HOST) service nfs-ganesha-gpfs restart || exit 1
-./runcthon --server $(HOST) --serverdir /ibm/fs0/hudson64/jenkins/$NODE_NAME --onlyv3
+ssh -tt root@$HOST service nfs-ganesha-gpfs restart || exit 1
+./runcthon --server $HOST --serverdir /ibm/fs0/hudson64/jenkins/$NODE_NAME --onlyv3
 sudo ./runcthon --unmountall
 
 
