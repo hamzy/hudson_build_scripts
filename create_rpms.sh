aa0ccaee8d3bd0597d600cf6baaa0d4c7bda6b93
Jeremy Bongio <jbongio@us.ibm.com>
2012-01-25 13:43:53 -0500

    removed strict compile for rpm builds. upstream has too many warnings for pnfs.

--- create_rpms.sh
+++ create_rpms.sh
@@ -14,7 +14,8 @@ DATE=`date +%Y%m%d%H%M`
 
 rm -f /tmp/*.rpm
 cd src
-export CONFIG_FLAGS="--enable-strict-compile --with-fsal=GPFS --enable-snmp-adm --enable-nlm --enable-stat-exporter --enable-nfs4-locks --with-rpcal=TIRPC"
+#export CONFIG_FLAGS="--enable-strict-compile --with-fsal=GPFS --enable-snmp-adm --enable-nlm --enable-stat-exporter --enable-nfs4-locks --with-rpcal=TIRPC"
+export CONFIG_FLAGS=" --with-fsal=GPFS --enable-snmp-adm --enable-nlm --enable-stat-exporter --enable-nfs4-locks --with-rpcal=TIRPC"
 autoreconf -i
 ./configure $CONFIG_FLAGS
 perl -pi -e "s/Release: .*/Release: $DATE.$GITVER/" nfs-ganesha.spec
