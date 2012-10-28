121f29030c01f2b7238f9f3e77aa15d542276c3e
Jeremy Bongio <jbongio@us.ibm.com>
2012-10-28 00:40:43 -0400

    Added scripts to run and parse pynfs.

---
+++ parse_pynfs.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+my $pynfs_output = $ARGV[0];
+
+# Print all FAILURE output
+open(IN, "<${pynfs_output}") or die "Could not open ${pynfs_output}.\n";
+my $failure = 0;
+my $totfail = 1;
+my $tottest = 1;
+my $allfailmessages = "";
+foreach(<IN>) {
+    my $line = $_;
+    if ($line =~ /.*FAILURE.*/) {
+        $allfailmessages .= "$line";
+        $failure = 1;
+    } elsif ($failure == 1) {
+	print $line[0];
+        if (substr($line, 0, 1) eq ' ') {
+            $allfailmessages .= "$line";
+        } else {
+            $failure = 0;
+        }
+    }
+    if ($line =~ /Command line asked for (\d+) of (\d+).*/) {
+        $tottest = $2;
+        $allfailmessages .= "$line";
+    }
+    if ($line =~ /Of those: (\d+) Skipped, (\d+) Failed, (\d+) Warned, (\d+).*/) {
+        $totfail = $2;
+#       print "s:${1}  f:${2}  w:${3}  p:${4}\n";
+        $allfailmessages .= "$line";
+    }
+}
+close(IN);
+
+
+if ($totfail > 0) {
+    print "<testsuite failures='1' name ='PYNFS' tests='1' time='0'><failure message='\n${allfailmessages}'/></testcase><system-out>${allfailmessages}</system-out></testsuite>";
+} else {
+    print "<testsuite failures='0' name ='PYNFS' tests='1' time='0'></testsuite>";
+}
---
+++ run_pynfs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+##
+## run_connectathon.sh
+##
+## Made by Sean Dague
+## Login   <sdague@orac>
+##
+## Started on  Fri Nov 19 11:12:16 2010 Sean Dague
+##
+
+source ./CONFIG
+
+SERVER=$1
+HOSTFS=$2
+
+$PWD = `pwd`
+# get rid of the test files
+rm -f $WORKSPACE/*.xml
+sudo rm -rf /tmp/pynfs4.0 /tmp/pynfs4.1
+
+echo "Building pynfs"
+if [[! -d /home/hudson/pynfs ]]; then
+    git clone $PYNFS_LOC /home/hudson/pynfs
+fi
+
+cd /home/hudson/pynfs/nfs4.0
+git pull
+python setup.py build
+mkdir -p /tmp/pynfs4.0
+sudo python testserver.py $SERVER:$HOSTFS --maketree all &> /tmp/pynfs4.0/root-results
+perl parse_pynfs.pl /tmp/pynfs4.0/root-results &> $WORKSPACE/pynfs4.0_results.xml
+
+cd /home/hudson/pynfs/nfs4.1
+git pull
+python setup.py build
+mkdir -p /tmp/pynfs4.1
+sudo python testserver.py $SERVER:$HOSTFS --maketree all &> /tmp/pynfs4.1/root-results
+perl parse_pynfs.pl /tmp/pynfs4.1/root-results &> $WORKSPACE/pynfs4.1_results.xml
+
+
